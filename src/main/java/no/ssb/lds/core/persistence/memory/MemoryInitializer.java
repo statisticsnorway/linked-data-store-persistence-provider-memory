567cb2f9cbf123e0c5a12bfb9f91f4c22466f4b1
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2018-12-07 18:19:41 +0100

    Use JsonDocument persistence-api.

--- src/main/java/no/ssb/lds/core/persistence/memory/MemoryInitializer.java
+++ src/main/java/no/ssb/lds/core/persistence/memory/MemoryInitializer.java
@@ -3,11 +3,15 @@ package no.ssb.lds.core.persistence.memory;
 import no.ssb.lds.api.persistence.PersistenceInitializer;
 import no.ssb.lds.api.persistence.ProviderName;
 import no.ssb.lds.api.persistence.TransactionFactory;
-import no.ssb.lds.api.persistence.streaming.Persistence;
+import no.ssb.lds.api.persistence.flattened.DefaultFlattenedPersistence;
+import no.ssb.lds.api.persistence.json.BufferedJsonPersistence;
+import no.ssb.lds.api.persistence.json.JsonPersistence;
 import no.ssb.lds.core.persistence.foundationdb.FoundationDBDirectory;
 
 import java.util.Map;
 import java.util.Set;
+
+import static java.util.Optional.ofNullable;
 
 @ProviderName("mem")
 public class MemoryInitializer implements PersistenceInitializer {
@@ -21,16 +25,18 @@ public class MemoryInitializer implements PersistenceInitializer {
     public Set<String> configurationKeys() {
         return Set.of(
                 "persistence.mem.wait.min",
-                "persistence.mem.wait.max"
+                "persistence.mem.wait.max",
+                "persistence.fragment.capacity"
         );
     }
 
     @Override
-    public Persistence initialize(String defaultNamespace, Map<String, String> configuration, Set<String> managedDomains) {
+    public JsonPersistence initialize(String defaultNamespace, Map<String, String> configuration, Set<String> managedDomains) {
         int waitMinMs = Integer.parseInt(configuration.get("persistence.mem.wait.min"));
         int waitMaxMs = Integer.parseInt(configuration.get("persistence.mem.wait.max"));
+        int fragmentCapacityBytes = Integer.parseInt(ofNullable(configuration.get("persistence.fragment.capacity")).orElse("8192"));
         TransactionFactory transactionFactory = new MemoryTransactionFactory();
         FoundationDBDirectory foundationDbDirectory = new MemoryDirectory(2);
-        return new MemoryPersistence(transactionFactory, foundationDbDirectory);
+        return new BufferedJsonPersistence(new DefaultFlattenedPersistence(new MemoryPersistence(transactionFactory, foundationDbDirectory), fragmentCapacityBytes), fragmentCapacityBytes);
     }
 }
--- src/test/java/no/ssb/lds/core/persistence/memory/MemoryPersistenceIntegrationTest.java
+++ src/test/java/no/ssb/lds/core/persistence/memory/MemoryPersistenceIntegrationTest.java
@@ -1,26 +1,24 @@
 package no.ssb.lds.core.persistence.memory;
 
-import no.ssb.lds.api.persistence.buffered.DefaultBufferedPersistence;
-import no.ssb.lds.core.persistence.test.BufferedPersistenceIntegration;
+import no.ssb.lds.core.persistence.test.PersistenceIntegrationTest;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.util.Map;
 import java.util.Set;
 
-public class MemoryPersistenceIntegrationTest extends BufferedPersistenceIntegration {
+public class MemoryPersistenceIntegrationTest extends PersistenceIntegrationTest {
 
     public MemoryPersistenceIntegrationTest() {
-        super("lds-provider-memory-integration-test", 8 * 1024);
+        super("lds-provider-memory-integration-test");
     }
 
     @BeforeClass
     public void setup() {
-        streaming = new MemoryInitializer().initialize(namespace,
+        persistence = new MemoryInitializer().initialize(namespace,
                 Map.of("persistence.mem.wait.min", "0",
                         "persistence.mem.wait.max", "0"),
                 Set.of("Person", "Address", "FunkyLongAddress"));
-        persistence = new DefaultBufferedPersistence(streaming, 8 * 1024);
     }
 
     @AfterClass
